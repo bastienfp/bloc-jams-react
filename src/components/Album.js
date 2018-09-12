410bacfb718fe6585512501e06781b0a8f31333b
bastienfp <bastien.fp@gmail.com>
2018-09-12 11:14:44 -0400

    Completed assignment Player Bar: Buttons

--- src/components/Album.js
+++ src/components/Album.js
@@ -54,6 +54,14 @@ class Album extends Component {
       this.play();
     }
 
+    handleNextClick(song) {
+      const currentIndex = this.state.album.songs.findIndex(song => this.state.currentSong === song);
+      const newIndex = Math.min(this.state.album.songs.length -1, currentIndex + 1);
+      const newSong = this.state.album.songs[newIndex];
+      this.setSong(newSong);
+      this.play();
+    }
+
     handleMouseEnter(song) {
       this.setState({ isHovered: song });
     }
@@ -106,6 +114,7 @@ class Album extends Component {
           currentSong={this.state.currentSong}
           handleSongClick={() => this.handleSongClick(this.state.currentSong)}
           handlePrevClick={() => this.handlePrevClick()}
+          handleNextClick={() => this.handleNextClick()}
         />
       </section>
     );
